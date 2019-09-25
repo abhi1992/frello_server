97367d358a01ffe22c343696a13031711e174549
Abhishek Banerjee <abhishekbanerjee1992@gmail.com>
2019-09-25 16:14:39 +0530

    lint fix

--- src/models/board.model.js
+++ src/models/board.model.js
@@ -1,18 +1,17 @@
 import mongoose from 'mongoose';
 
-const Any = new mongoose.Schema({ any: Object });
 const boardSchema = mongoose.Schema({
   title: String,
   taskListIds: Array,
   background: {
-      bg_type:  String,
-      data: {
-        color: String,
-      },
+    bg_type: String,
+    data: {
+      color: String,
+    },
   },
   deleted: Number,
 }, {
-  timestamps: true
+  timestamps: true,
 });
 
 export default mongoose.model('Board', boardSchema);
